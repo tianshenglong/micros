729a68c642713fec49cff7850bca4f311042b3a3
tianshenglong <tianshenglong@foxgoing.com>
2020-12-12 14:39:00 +0800

    init commit

--- gateway/src/main/java/com/study/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/study/gateway/GatewayApplication.java
@@ -2,12 +2,46 @@ package com.study.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .route("hystrix-route", p -> p
+                        .host("*.abc.com")  //根据host路由
+                        .filters(f -> f
+                                .hystrix(config -> config
+                                        .setName("mycmd")
+                                        .setFallbackUri("forward:/fallback")))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+
+    /**
+     * 该方法对应于上面的路由hystrix-route的fallback。
+     * Mono是webflux的类，响应式编程的写法
+     * @return
+     */
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
     }
 
 }
--- gateway/src/test/java/com/study/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/study/gateway/GatewayApplicationTests.java
@@ -3,10 +3,8 @@ package com.study.gateway;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GatewayApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
